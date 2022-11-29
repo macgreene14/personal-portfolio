212dbfe2bd16ca9d4caf2bb2730550b0620cb892
jmg <macgreene14@gmail.com>
2022-11-29 12:12:45 -0500

    swapped about-me route to default

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Navigate to="about-me" />} />
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/" element={<AboutMe />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
