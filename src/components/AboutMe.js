3f37d919d01e44576dc6049c1acce0cc6cd12c62
jmg <macgreene14@gmail.com>
2022-12-13 20:24:52 -0500

    revised about-me font

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
         <h1 className="m-4 text-2xl font-bold tracking-tight sm:text-center sm:text-4xl">
           About Me
         </h1>
-        <p className="m-8 text-xl leading-12 text-gray-600 sm:text-center">
+        <p className="m-8 text-xl leading-12 text-black-600 sm:text-center">
           Mac Greene is an aspiring web developer based out of Nashville, TN. He
           received a B.S. in Engineering from Fort Lewis College in Durango,
           Colorado. Since graduating, Mac has worked as a project engineering
